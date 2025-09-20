0def9ebaa6ce2971c224a8a2ed464d26f4ef3d69
sujeet kumar <sujeetkumar2010@live.com>
2025-09-20 07:02:40 +0530

    ClientProject Added

---
+++ src/ToyRobotSimulator.Client/Program.cs
@@ -0,0 +1,33 @@
+﻿using ToyRobotSimulator.Core.Interfaces;
+using ToyRobotSimulator.Services.Implementation;
+using ToyRobotSimulator.Services.Services;
+
+namespace ToyRobotSimulator.Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ITable table = new Table(5, 5);
+            IRobot robot = new Robot(table);
+            var parser = new CommandParser(robot, Console.WriteLine);
+
+            IEnumerable<string> lines;
+
+            if (args.Length > 0 && File.Exists(args[0]))
+                lines = File.ReadAllLines(args[0]);
+            else
+            {
+                Console.WriteLine("Enter commands (empty line to finish):");
+                var input = new List<string>();
+                string? line;
+                while (!string.IsNullOrEmpty(line = Console.ReadLine()))
+                    input.Add(line);
+                lines = input;
+            }
+
+            foreach (var line in lines)
+                parser.Parse(line)?.Execute();
+        }
+    }
+}
--- src/ToyRobotSimulator.Services/Implementation/Table.cs
+++ src/ToyRobotSimulator.Services/Implementation/Table.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ToyRobotSimulator.Core.Interfaces;
 using ToyRobotSimulator.Core.Models;
 
 namespace ToyRobotSimulator.Services.Implementation
 {
-    public class Table
+    public class Table: ITable
     {
-            private readonly int _width;
+        private readonly int _width;
         private readonly int _height;
 
         public Table(int width = 5, int height = 5)
